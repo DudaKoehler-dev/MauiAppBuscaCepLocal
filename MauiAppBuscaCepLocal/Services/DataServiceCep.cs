6e029ea0faa9d9c5dd6026e3440cc7bb7560883a
DudaKohler-dev <eduardakoehler6@gmail.com>
2024-10-03 14:47:08 -0300

    novo

--- MauiAppBuscaCepLocal/Services/DataServiceCep.cs
+++ MauiAppBuscaCepLocal/Services/DataServiceCep.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MauiAppBuscaCepLocal.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace MauiAppBuscaCepLocal.Services
 {
     internal class DataServiceCep
     {
+        public static async Task<Endereco?> GetEnderecoByCep(string cep)
+        {
+            Endereco? end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                // 10.0.2.2
+                HttpResponseMessage response = await client.GetAsync("http://10.0.2.2:8000/endereco/by-cep?cep=" + cep);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return end;
+        }
     }
 }
